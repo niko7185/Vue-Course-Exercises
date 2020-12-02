80f7cff96c5b4a44c4f00bfc98c3b145b945028c
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2020-12-02 11:40:41 +0100

    adding assignment 1

---
+++ app.js
@@ -0,0 +1,22 @@
+
+Vue.createApp({
+	data(){
+		return {
+			name: "Nikolaj",
+			age: 20,
+			imageUrl: "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
+		};
+	},
+	methods: {
+		
+		inFiveYears() {
+			return this.age + 5;
+		},
+		randomNumber(){
+			
+			return Math.random();
+		}
+	}
+	
+}).mount("#assignment");
+
