6f38ffd10c9986597aceb431abf4efd6f8b022e0
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2020-12-02 11:43:53 +0100

    adding assignment 1 to new branch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ Vue.createApp({
 		return {
 			name: "Nikolaj",
 			age: 20,
-			imageUrl: "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
+			imageUrl: "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0",
 		};
 	},
 	methods: {
